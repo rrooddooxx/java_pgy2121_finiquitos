6bfabb422ecb8ba95e07a43fb5df2dfb83ec76e5
ponytaKXN <ju.valdesv@duocuc.cl>
2022-11-12 21:30:56 -0300

    oli

--- src/main/java/org/ponyseba/Main.java
+++ src/main/java/org/ponyseba/Main.java
@@ -3,5 +3,6 @@ package org.ponyseba;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hola planeta tierra!");
     }
 }
