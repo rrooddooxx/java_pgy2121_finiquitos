87a35f2fd9ad9dbcbf5230c5f1f4face2ed50502
Sebastián Kravetz <sebastiankravetz@icloud.com>
2022-11-22 17:58:40 -0300

    [@wwiiddeeweb] excel export first commit

--- FiniquitosCalc/src/main/java/com/ponyseba/finiquitoscalc/controller/ExcelController.java
+++ FiniquitosCalc/src/main/java/com/ponyseba/finiquitoscalc/controller/ExcelController.java
@@ -4,12 +4,31 @@
  */
 package com.ponyseba.finiquitoscalc.controller;
 
+import ch.qos.logback.core.Context;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import model.Finiquito;
+import org.jxls.util.JxlsHelper;
+
 /**
  *
  * @author sebastiankravetz
  */
 public class ExcelController {
     
-    // https://jxls.sourceforge.net/samples/object_collection.html
+    public void generarArchivoXls(Finiquito finiquito){
+           List<Finiquito> listaFiniquitos = new ArrayList<>();
+           listaFiniquitos.add(finiquito)
+    try(InputStream is = ObjectCollectionDemo.class.getResourceAsStream("object_collection_template.xls")) {
+        try (OutputStream os = new FileOutputStream("target/object_collection_output.xls")) {
+            Context context = new Context();
+            context.putVar("employees", employees);
+            JxlsHelper.getInstance().processTemplate(is, os, context);
+        }
+    }
+    }
     
 }
